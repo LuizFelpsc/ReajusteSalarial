b9230c60737da0292267a07a3cd25aa05dd47612
Luiz Felipe <117685654+LuizFelpsc@users.noreply.github.com>
2022-11-15 13:04:48 -0300

    Delete ReajusteSalarial15%.py

--- ReajusteSalarial15%.py
+++
@@ -1,6 +0,0 @@
-# Reajuste Salarial de 15%
-
-s = float(input("Qual e o salário De Um Funcionário?"))
-a = s + (s * 15 / 100)
-print("O Salário Era De R${:.2f}, Com Aumento De 15% Passa a Receber R${:.2f}".format(s, a))
-
